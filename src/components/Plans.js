feeff345a59086850af5977b1ba5ee82fc8226cb
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-11-10 15:45:36 +0530

    Implemented plans Listing and Creating Components

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
+<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
+=======
+import 'bootstrap/dist/css/bootstrap.min.css';
+>>>>>>> 0ece037 (Implemented plans Listing and Creating Components)
 import { FaEdit } from "react-icons/fa";
 import { MdAdd } from "react-icons/md";
 import PlansModal from "./modals/PlansModal";
 
 function Plans() {
+<<<<<<< HEAD
   const [plans, setPlans] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   useEffect(() => {
@@ -69,3 +74,66 @@ function Plans() {
   );
 }
 export default Plans;
+=======
+    const [plans, setPlans] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+
+
+
+    useEffect(() => {
+        getPlansDetails();
+    }, [])
+    const getPlansDetails = async () => {
+        if (!openModal) {
+            let plans = await fetch('http://3.111.147.217:3000/plans');
+            plans = await plans.json();
+            setPlans(plans);
+        }
+    }
+
+    console.log("Plans", plans)
+    return (
+        <div className="batch-list">
+            <h3 className="batch-heading">Plans <button onClick={() => { setOpenModal(true) }}>{<MdAdd />}</button>{openModal && <PlansModal closeModal={setOpenModal} />}</h3>
+            <div class="table-batch-list">
+                <table className="table batch-list">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Plan Name</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Price</th>
+                            <th>Tag</th>
+                            <th>Type</th>
+                            <th>Duration</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            // plans?.map((item, index) => {
+                            //     return (
+                            //         <tr>
+                            //             <td>{index + 1}</td>
+                            //             <td>{item.name}</td>
+                            //             <td>{item.description}</td>
+                            //             <td>{item.status}</td>
+                            //             <td>{item.price}</td>
+                            //             <td>{item.tag}</td>
+                            //             <td>{item.type}</td>
+                            //             <td>{item.duration}</td>
+                            //             <td ><button onClick={() => { alert("Edit Coach") }}>{<FaEdit />}</button></td>
+
+                            //         </tr>
+                            //     )
+                            // })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+export default Plans
+>>>>>>> 0ece037 (Implemented plans Listing and Creating Components)
