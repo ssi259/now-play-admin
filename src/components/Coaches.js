5c66d735eb5f149a186453ff48bd3de1dce85d96
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-11-17 17:06:22 +0530

    Implemented Coach update form

--- src/components/Coaches.js
+++ src/components/Coaches.js
@@ -1,62 +1,91 @@
-import React, { useState,useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { FaEdit } from "react-icons/fa";
 import { MdAdd } from "react-icons/md";
-
+import UpdateCoach from "./modals/UpdateCoach";
 import CoachModal from "./modals/CoachModal";
 
-function Coaches(){
-    const[coaches,setCoaches] = useState([]);
-    const[openModal,setOpenModal] = useState(false);
+function Coaches() {
+  const [coaches, setCoaches] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+  const [editData, setEditData] = useState({});
 
-    useEffect(()=>{
-        getCoachDetails();
-    },[openModal])
-    const getCoachDetails=async()=>{
-        if(!openModal){
-            let coaches = await fetch('http://3.111.147.217:3000/coach');
-            coaches = await coaches.json();
-            setCoaches(coaches.data);
-        }
+  useEffect(() => {
+    getCoachDetails();
+  }, [openModal,editModal]);
+  const getCoachDetails = async () => {
+    if (!openModal) {
+      let coaches = await fetch("http://3.111.147.217:3000/coach");
+      coaches = await coaches.json();
+      setCoaches(coaches.data);
     }
-    console.log("coaches",coaches)
-    return(
-        <div className="batch-list">
-            <h3 className="batch-heading">Coach<button i onClick={()=>{setOpenModal(true)}}>{<MdAdd/>}</button>{openModal && <CoachModal closeModal= {setOpenModal}/>}</h3>
-            <div class = "table-batch-list">
-                <table className="table batch-list">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>id</th>
-                            <th>Coach Name</th>
-                            <th>Phone</th>
-                            <th>Sport</th>
-                            <th>Experience(months)</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    
-                    {
-                        coaches.map((item, index) => {
-                        return (
-                                <tr>
-                                    <th>{index+1}</th>
-                                    <th>{item.id}</th>
-                                    <td>{item.name}</td>
-                                    <th>{item.phone_number}</th>
-                                    <td>{item.sports_id}</td>
-                                    <td>{item.experience}</td>
-                                    <td ><button  onClick={()=>{alert("Edit Coach")}}>{<FaEdit/>}</button></td>
-
-                                </tr>
-                        )
-                    })}
-                    </tbody>
-                </table>
-            </div>
-        </div>  
-    )
+  };
+  return (
+    <div className="batch-list">
+      <h3 className="batch-heading">
+        Coach
+        <button
+          i
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          {<MdAdd />}
+        </button>
+        {openModal && <CoachModal closeModal={setOpenModal} />}
+        {editModal && (
+          <UpdateCoach closeEditModal={setEditModal} editData={editData} />
+        )}
+      </h3>
+      <div class="table-batch-list">
+        <table className="table batch-list">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>id</th>
+              <th>Coach Name</th>
+              <th>Phone</th>
+              <th>Sport</th>
+              <th>Experience(months)</th>
+              <th>Email</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Pincode</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coaches.map((item, index) => {
+              return (
+                <tr>
+                  <th>{index + 1}</th>
+                  <th>{item.id}</th>
+                  <td>{item.name}</td>
+                  <th>{item.phone_number}</th>
+                  <td>{item.sports_id}</td>
+                  <td>{item.experience}</td>
+                  <td>{item.email}</td>
+                  <td>{item.city}</td>
+                  <td>{item.state}</td>
+                  <td>{item.pincode}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setEditModal(true);
+                        setEditData(item);
+                      }}
+                    >
+                      {<FaEdit />}
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
-export default Coaches
+export default Coaches;
---
+++ src/components/modals/UpdateCoach.js
@@ -0,0 +1,210 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const UpdateCoach = ({ closeEditModal, editData }) => {
+  const [data, setData] = useState([]);
+  const [sports, setSports] = useState([]);
+  useEffect(() => {
+    setData(editData);
+    getSportsDetails();
+  }, [editData]);
+  const getSportsDetails = async () => {
+    let sports = await fetch("http://3.111.147.217:3000/sports");
+    sports = await sports.json();
+    setSports(sports);
+  };
+  const handle = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const submit = async (e) => {
+    e.preventDefault();
+    await axios
+      .put(`http://3.111.147.217:3000/coach/${editData.id}`, data)
+      .then((res) => {
+        alert("Coach Updated Successfully");
+      });
+    {
+      closeEditModal(false);
+    }
+  };
+
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => {
+              closeEditModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div>
+          <h1>Update Coach</h1>
+        </div>
+
+        <form>
+          <div className="overlay">
+            <div class="form-group row">
+              <label for="coach-name" class="col-sm-2 label">
+                Name
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  id="name"
+                  class="form-control"
+                  value={data.name}
+                  name="name"
+                  placeholder="coach Name"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="coach-email" class="col-sm-2 label">
+                Email
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  id="email"
+                  class="form-control"
+                  value={data.email}
+                  name="email"
+                  placeholder="coach Email"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="coach-phn" class="col-sm-2 label">
+                Phone
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  id="phone"
+                  class="form-control"
+                  value={data.phone_number}
+                  name="phone"
+                  placeholder="Phone Number"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="coach-name" class="col-sm-2 label">
+                Sport
+              </label>
+              <div class="col-sm-10">
+                <select
+                  class="form-control"
+                  name="sport_id"
+                  value={data.sports_id}
+                  onChange={(e) => handle(e)}
+                >
+                  <option class="form-control" name="sport_id">
+                    {" "}
+                    -- Select a sport --{" "}
+                  </option>
+                  {sports.map((sport) => (
+                    <option
+                      class="form-control"
+                      value={sport.id}
+                      name="sport_id"
+                    >
+                      {sport.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="experience" class="col-sm-2 label">
+                Exp
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  name="experience"
+                  class="form-control"
+                  id="experience"
+                  placeholder="Experience in months"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="about-coach" class="col-sm-2 label">
+                About
+              </label>
+              <div class="col-sm-10">
+                <textarea
+                  onChange={(e) => handle(e)}
+                  name="about"
+                  class="form-control"
+                  id="about-coach"
+                  placeholder="About Coach"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="locality" class="col-sm-2 label">
+                Locality
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  name="locality"
+                  class="form-control"
+                  id="locality"
+                  placeholder="Locality"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="city" class="col-sm-2 label">
+                City
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  name="city"
+                  class="form-control"
+                  id="city"
+                  placeholder="city"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="city" class="col-sm-2 label">
+                Pincode
+              </label>
+              <div class="col-sm-10">
+                <input
+                  onChange={(e) => handle(e)}
+                  name="pincode"
+                  class="form-control"
+                  id="pincode"
+                  placeholder="pincode"
+                />
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <div class="col-sm-10">
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  onClick={(e) => submit(e)}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateCoach;
