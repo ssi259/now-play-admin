7449c58d42ef9c2e755ccfacb5ca811f76a30307
aalesh01-hetu <117704212+aalesh01-hetu@users.noreply.github.com>
2022-11-18 18:08:20 +0530

    Aalesh/update academy form (#15)
    * implemented update_academy form
    
    * working on updateAcademy
    
    * Implemented update Academy  form
    
    * useEffect issue resolved
    
    * removed console log's and reformatted code
    
    * update listing attributes
    
    * pre populate phone number fix
    
    * prepopulate select filed

--- src/components/Academies.js
+++ src/components/Academies.js
@@ -1,57 +1,80 @@
-import React, { useState,useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { FaEdit } from "react-icons/fa";
 import { MdAdd } from "react-icons/md";
-
 import AcademiesModal from "./modals/AcademiesModal";
+import UpdateAcademy from "./modals/UpdateAcademy";
 
+function Academies() {
+  const [Academies, setAcademies] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+  const [editData, setEditData] = useState({});
 
-function Academies(){
-    const[Academies,setAcademies] = useState([]);
-    const[openModal,setOpenModal] = useState(false);
-
-    useEffect(()=>{
-        getAcademyDetails();
-    },[openModal])
-    const getAcademyDetails=async()=>{
-        let academies = await fetch('http://3.111.147.217:3000/academies');
-        academies = await academies.json();
-        setAcademies(academies);
-    }
-    console.log("Academies",Academies)
-    return(
-        <div className="batch-list">
-             <h3 className="batch-heading">Academies<button i onClick={()=>{setOpenModal(true)}}>{<MdAdd/>}</button>{openModal && <AcademiesModal closeModal= {setOpenModal}/>}</h3>
-            <div class = "table-batch-list">
-                <table className="table batch-list">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>id</th>
-                            <th>Academy Name</th>
-                            <th>Sport</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    
-                    {
-                        Academies.map((item, index) => {
-                        return (
-                                <tr>
-                                    <th>{index+1}</th>
-                                    <th>{item.id}</th>
-                                    <td>{item.name}</td>
-                                    <td>{item.sports_name}</td>
-                                    <td ><button onClick={()=>{alert("Edit Academy")}}>{<FaEdit/>}</button></td>
-
-                                </tr>
-                        )
-                    })}
-                    </tbody>
-                </table>
-            </div>
-        </div>  
-    )
+  useEffect(() => {
+    getAcademyDetails();
+  }, [openModal, editModal]);
+  const getAcademyDetails = async () => {
+    let batches = await fetch("http://3.111.147.217:3000/academies");
+    batches = await batches.json();
+    setAcademies(batches);
+  };
+  return (
+    <div className="batch-list">
+      <h3 className="batch-heading">
+        Academies
+        <button
+          i
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          {<MdAdd />}
+        </button>
+        {openModal && <AcademiesModal closeModal={setOpenModal} />}
+        {editModal && (
+          <UpdateAcademy closeModal={setEditModal} editData={editData} />
+        )}
+      </h3>
+      <div class="table-batch-list">
+        <table className="table batch-list">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>id</th>
+              <th>Academy Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Sport</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Academies.map((item, index) => {
+              return (
+                <tr>
+                  <th>{index + 1}</th>
+                  <th>{item.id}</th>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone_number}</td>
+                  <td>{item.sports_name}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setEditModal(true), setEditData(item);
+                      }}
+                    >
+                      {<FaEdit />}
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
-export default Academies
+export default Academies;
