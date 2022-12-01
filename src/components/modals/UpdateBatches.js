f3eced72604994e7bf1638d9897122421658d93b
dhanush-hetulabs <dhanush.kumar@hetu-labs.com>
2022-12-01 15:18:46 +0530

    added update form

--- src/components/modals/UpdateBatches.js
+++ src/components/modals/UpdateBatches.js
@@ -67,7 +67,7 @@ const UpdateBatches = ({ closeModal, batchData }) => {
     getArenaDetails();
     getCoachDetails();
     onUpdateCheckbox();
-  }, []);
+  }, [closeModal]);
 
   console.log("data", data);
 
