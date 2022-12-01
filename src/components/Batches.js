f73ef322d81b3b635494047804fe48a77da65d85
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-12-01 16:54:16 +0530

    removed extra lines

--- src/components/Batches.js
+++ src/components/Batches.js
@@ -52,10 +52,7 @@ function Batches() {
   const getBatchDetails = async () => {
     let batches = await fetch(
       `${process.env.REACT_APP_API_PATH}
-
-
-
-/batches/search?lat=28.21&&lng=78.12`
+      /batches/search?lat=28.21&&lng=78.12`
     );
     batches = await batches.json();
     for (var i = 0; i < batches.batchList.length; i++) {
