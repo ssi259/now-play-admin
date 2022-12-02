3584a9ebac544e8e350ae728de6ed9539968c871
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-12-02 16:02:56 +0530

    fixed urls

--- src/components/Coaches.js
+++ src/components/Coaches.js
@@ -21,7 +21,7 @@ function Coaches() {
 
   const getCoachDetails = async () => {
     if (!openModal) {
-      let coaches = await fetch("http://65.0.72.215:3000/coach");
+      let coaches = await fetch("http://3.111.147.217:3000/coach");
       coaches = await coaches.json();
       setCoaches(coaches.data);
       console.log("coaches", coaches.data);
@@ -29,14 +29,14 @@ function Coaches() {
 }
 
     const getSportsDetails = async () => {
-      let sports = await fetch("http://65.0.72.215:3000/sports");
+      let sports = await fetch("http://3.111.147.217:3000/sports");
       sports = await sports.json();
       Sports.current = sports;
     };
 
     const updateCoachStatus = async (id, status) => {
       console.log(id, status);
-      axios.put(`http://65.0.72.215:3000/coach/${id}`, {
+      axios.put(`http://3.111.147.217:3000/coach/${id}`, {
         status: status
       })
         .then(res => {
