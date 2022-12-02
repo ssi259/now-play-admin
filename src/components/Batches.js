914a4a0f37c4d139545055523dfce1fb60757efc
dhanush-hetulabs <117707833+dhanush-hetulabs@users.noreply.github.com>
2022-12-02 20:09:54 +0530

    Dhanush/update batch (#41)
    * added update form
    
    * update_batches_compeled
    
    * overide for Admin getAllBatches
    
    * admin_override_completed

--- src/components/Batches.js
+++ src/components/Batches.js
@@ -48,7 +48,7 @@ function Batches() {
   }
   const getBatchDetails = async () => {
     let batches = await fetch(
-      "http://3.111.147.217:3000/batches/search?lat=28.21&&lng=78.12"
+      "http://3.111.147.217:3000/batches/search?lat=28.21&&lng=78.12&&type=admin"
     );
     batches = await batches.json();
     for (var i = 0; i < batches.batchList.length; i++) {
