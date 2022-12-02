9db56a90e8e1aad401df02a5e6300d6158a77e53
aalesh01-hetu <117704212+aalesh01-hetu@users.noreply.github.com>
2022-12-02 13:44:46 +0530

    Aalesh / file upload for batches (#9)
    * Implemented plans Listing and Creating Components
    
    * code reformatted
    
    * made PR review changes
    
    * updated to select multiple images
    
    * solve merge conflicts

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaEdit } from "react-icons/fa";
 import { MdAdd } from "react-icons/md";
 import PlansModal from "./modals/PlansModal";
