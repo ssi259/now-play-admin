fb325683a8b81260121557f04335b6fead642ebe
dhanush-hetulabs <dhanush.kumar@hetu-labs.com>
2022-12-25 13:36:49 +0530

    sport and update sports validation done

--- src/components/modals/SportModal.js
+++ src/components/modals/SportModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from 'axios';
 function SportModal({closeModal}){
     const url = `${process.env.REACT_APP_API_PATH}/sports`;
@@ -7,6 +7,8 @@ function SportModal({closeModal}){
         type:"",
         about:""
     })
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
     function handle(e){
         const newData = {...data}
         newData[e.target.id] = e.target.value
@@ -15,6 +17,9 @@ function SportModal({closeModal}){
     }
     async function submit(e){
         e.preventDefault();
+        await setFormErrors(validate(data));
+        setIsSubmit(true);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
         await Axios.post(url,{
             name: data.name,
             type: data.type,
@@ -26,8 +31,23 @@ function SportModal({closeModal}){
             alert(err.response.data.details)
         })
         {closeModal(false)}
+    }
 
     }
+    const validate = (values) => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = "Name is required!";
+        }
+        if (!values.type) {
+            errors.type = "Type is required!";
+        }
+        if (!values.about) {
+            errors.about = "About is required!";
+        }
+        
+        return errors;
+      };
     return(
         
         <div className = "modalBackground">
@@ -43,18 +63,21 @@ function SportModal({closeModal}){
                         <label for="sportName" class="col-sm-2 label">Name</label>
                         <div class="col-sm-10">
                         <input onChange={(e)=>handle(e)} id = "name" class="form-control" value= {data.name} name = "name" placeholder="sport Name"/>
+                        <span>{formErrors.name}</span>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="sportType" class="col-sm-2 label">Type</label>
                         <div class="col-sm-10">
                         <input onChange={(e)=>handle(e)} name="type" class="form-control" id="type" placeholder="sport Type"/>
+                        <span>{formErrors.type}</span>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="aboutSport" class="col-sm-2 label">About</label>
                         <div class="col-sm-10">
                         <textarea onChange={(e)=>handle(e)} name="about" class="form-control" id="about" placeholder="About Sport"/>
+                        <span>{formErrors.about}</span>
                         </div>
                     </div>
                     <div class="form-group row">
